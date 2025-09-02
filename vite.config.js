153701bfaac0529fa349576a021fbfa529511907
Job-Wilhelm <127032967+Job-Wilhelm@users.noreply.github.com>
2025-09-03 00:21:19 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/sportifyplus-web/'
+  base: '/sportifyplus-web/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
